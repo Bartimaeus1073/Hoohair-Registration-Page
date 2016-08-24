c9a13bfa9fdf780d90212ce448f06fe45c19575c
Toma Alexandru <bartimaeus1073@gmail.com>
2016-08-24 13:31:54 +0300

    Managed to solve transitions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-angular.module("Hoohair", [])
+var app = angular.module("Hoohair", ["ngAnimate"]);
 
-.controller("formCtrl", function($scope) {
+app.controller("formCtrl", function($scope) {
   $scope.registered = false;
   
   $scope.register = function() {
